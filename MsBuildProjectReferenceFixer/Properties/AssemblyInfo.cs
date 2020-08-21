d3b19d07103420c64f95020e63ad4d4f7006d4a2
Ace Olszowka <aolszowka@gmail.com>
2020-08-21 12:28:51 -0600

    Converted to NDesk.Options for options parsing

--- MsBuildProjectReferenceFixer/Properties/AssemblyInfo.cs
+++ MsBuildProjectReferenceFixer/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MsBuildProjectReferenceFixer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/aolszowka/MsBuildProjectReferenceFixer")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Ace Olszowka")]
+[assembly: AssemblyCompany("Ace Olszowka (GitHub @aolszowka)")]
 [assembly: AssemblyProduct("MsBuildProjectReferenceFixer")]
 [assembly: AssemblyCopyright("Copyright © Ace Olszowka 2018-2020")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.3")]
-[assembly: AssemblyFileVersion("1.0.0.3")]
+[assembly: AssemblyVersion("1.0.0.4")]
+[assembly: AssemblyFileVersion("1.0.0.4")]
